17cac25070e01b40246e56d48103ca0e5bbd3e44
fdjyqjc5z-s <2843656112@qq.com>
2026-05-01 02:54:39 +0800

    server: 挂载任务提交路由

--- server/src/routes/taskRoutes.js
+++ server/src/routes/taskRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { requireAuth } from '../middleware/auth.js';
-import { acceptTask, getMyTaskAccepts, getTaskDetail, listOpenTasks } from '../controllers/taskController.js';
+import { acceptTask, getMyTaskAccepts, getTaskDetail, listOpenTasks, submitTaskProgress } from '../controllers/taskController.js';
 
 const router = Router();
 
 router.get('/tasks', listOpenTasks);
 router.get('/tasks/:id', getTaskDetail);
 router.post('/tasks/:id/accept', requireAuth, acceptTask);
+router.post('/accepts/:acceptId/submit', requireAuth, submitTaskProgress);
 router.get('/my/accepts', requireAuth, getMyTaskAccepts);
 
 export default router;
