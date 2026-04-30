78d839f1d71105e48b1ac72c844d3af301a1ebb2
fdjyqjc5z-s <2843656112@qq.com>
2026-05-01 00:48:51 +0800

    web: 添加神秘客用户端主布局

---
+++ web/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-shell">
+    <div class="mystery-orb orb-a"></div>
+    <div class="mystery-orb orb-b"></div>
+
+    <main class="page-wrap">
+      <router-view />
+    </main>
+
+    <nav class="bottom-nav">
+      <RouterLink to="/" class="nav-item">首页</RouterLink>
+      <RouterLink to="/tasks" class="nav-item">任务</RouterLink>
+      <RouterLink to="/shop" class="nav-item">商城</RouterLink>
+      <RouterLink to="/mine" class="nav-item">我的</RouterLink>
+    </nav>
+  </div>
+</template>
