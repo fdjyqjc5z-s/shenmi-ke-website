4852ff932e536e461fda835aedd54c7491da499b
fdjyqjc5z-s <2843656112@qq.com>
2026-05-01 14:56:26 +0800

    feat: wire invite binding route

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { requireAuth } from '../middleware/auth.js';
-import { getMyAssets, getMyInviteInfo, getMyProfile } from '../controllers/userController.js';
+import { bindMyInviteCode, getMyAssets, getMyInviteInfo, getMyProfile } from '../controllers/userController.js';
 import { createWithdrawOrder, getMyWithdrawOrders } from '../controllers/withdrawController.js';
 
 const router = Router();
@@ -10,6 +10,7 @@ router.use(requireAuth);
 router.get('/me', getMyProfile);
 router.get('/assets', getMyAssets);
 router.get('/invite', getMyInviteInfo);
+router.post('/bind-invite', bindMyInviteCode);
 router.post('/withdraw-orders', createWithdrawOrder);
 router.get('/withdraw-orders', getMyWithdrawOrders);
 
