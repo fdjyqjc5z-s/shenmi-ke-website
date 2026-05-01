2fb8fa607913a4c803ed3c7522e6dd6bad452fbe
fdjyqjc5z-s <2843656112@qq.com>
2026-05-01 16:20:22 +0800

    user: wire address book routes

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { requireAuth } from '../middleware/auth.js';
 import { bindMyInviteCode, getMyAssets, getMyInviteInfo, getMyProfile } from '../controllers/userController.js';
 import { createWithdrawOrder, getMyWithdrawOrders } from '../controllers/withdrawController.js';
+import { createMyAddress, deleteMyAddress, listMyAddresses, updateMyAddress } from '../controllers/userAddressController.js';
 
 const router = Router();
 
@@ -11,6 +12,12 @@ router.get('/me', getMyProfile);
 router.get('/assets', getMyAssets);
 router.get('/invite', getMyInviteInfo);
 router.post('/bind-invite', bindMyInviteCode);
+
+router.get('/addresses', listMyAddresses);
+router.post('/addresses', createMyAddress);
+router.put('/addresses/:id', updateMyAddress);
+router.delete('/addresses/:id', deleteMyAddress);
+
 router.post('/withdraw-orders', createWithdrawOrder);
 router.get('/withdraw-orders', getMyWithdrawOrders);
 
